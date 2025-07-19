5f90b03fe789a2c8f48b695aa65d2c031a9c9d09
Yi (Roben) Zhang <yi.zhang@bigtincan.com>
2025-07-19 18:14:02 +1000

    feat: Refine Header layout for desktop view to left-align navigation items

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -10,18 +10,18 @@ const Header = () => {
 
   return (
     <header className="bg-white shadow-md p-4 sticky top-0 z-50 w-full">
-      <nav className="flex gap-6 text-lg font-medium text-gray-700">
-        <Link to="/" className="hover:text-blue-500">
-          <Home className="inline-block mr-1" /> Home
+      <nav className="flex flex-wrap justify-around items-center text-lg font-medium text-gray-700 sm:flex-nowrap sm:justify-start sm:gap-6">
+        <Link to="/" className="flex flex-col items-center text-center flex-1 hover:text-blue-500 sm:flex-row">
+          <Home className="mb-1 sm:mr-1 sm:mb-0" /> Home
         </Link>
-        <Link to="/menu" className="hover:text-blue-500">
-          <Utensils className="inline-block mr-1" /> Menu
+        <Link to="/menu" className="flex flex-col items-center text-center flex-1 hover:text-blue-500 sm:flex-row">
+          <Utensils className="mb-1 sm:mr-1 sm:mb-0" /> Menu
         </Link>
-        <Link to="/cart" className="hover:text-blue-500">
-          <ShoppingCart className="inline-block mr-1" /> Cart ({totalQty})
+        <Link to="/cart" className="flex flex-col items-center text-center flex-1 hover:text-blue-500 sm:flex-row">
+          <ShoppingCart className="mb-1 sm:mr-1 sm:mb-0" /> Cart ({totalQty})
         </Link>
-        <Link to="/success" className="hover:text-blue-500">
-          <CheckCircle className="inline-block mr-1" /> Success
+        <Link to="/success" className="flex flex-col items-center text-center flex-1 hover:text-blue-500 sm:flex-row">
+          <CheckCircle className="mb-1 sm:mr-1 sm:mb-0" /> Success
         </Link>
       </nav>
     </header>
